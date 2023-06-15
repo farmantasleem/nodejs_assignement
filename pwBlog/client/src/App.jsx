e132d2dbe919b62467e5962abacd176c62eda803
Farman Tasleem <iamfarmanmalik@gmail.com>
2023-06-15 17:06:18 +0530

    add blog component built

--- pwBlog/client/src/App.jsx
+++ pwBlog/client/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Addblog } from "./Pages/AddBlog";
 
 function App() {
   return (
@@ -8,7 +9,51 @@ function App() {
         <button>Blog</button>
         <button>About</button>
       </header>
+      <Addblog/>
       <main>
+
+      <div className="storyCard">
+          <img src="https://img.staticmb.com/mbcontent/images/uploads/2022/7/Image-of-colorful-sky-from-nandi-hills-bangalore.jpg" alt="" />
+          <div className="content">
+            <h1>Blog : How JavaScript is doing it</h1>
+            <p>
+              Short Description of Story : Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Eius nostrum minima quod facilis,
+              aperiam assumenda praesentium saepe nobis delectus? Consectetur
+              molestias eaque exercitationem. Excepturi quidem eveniet mollitia
+              ad dolorem iste.
+            </p>
+            <button>Read</button>
+          </div>
+        </div>
+        <div className="storyCard">
+          <img src="https://img.staticmb.com/mbcontent/images/uploads/2022/7/Image-of-colorful-sky-from-nandi-hills-bangalore.jpg" alt="" />
+          <div className="content">
+            <h1>Blog : How JavaScript is doing it</h1>
+            <p>
+              Short Description of Story : Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Eius nostrum minima quod facilis,
+              aperiam assumenda praesentium saepe nobis delectus? Consectetur
+              molestias eaque exercitationem. Excepturi quidem eveniet mollitia
+              ad dolorem iste.
+            </p>
+            <button>Read</button>
+          </div>
+        </div>
+        <div className="storyCard">
+          <img src="https://img.staticmb.com/mbcontent/images/uploads/2022/7/Image-of-colorful-sky-from-nandi-hills-bangalore.jpg" alt="" />
+          <div className="content">
+            <h1>Blog : How JavaScript is doing it</h1>
+            <p>
+              Short Description of Story : Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Eius nostrum minima quod facilis,
+              aperiam assumenda praesentium saepe nobis delectus? Consectetur
+              molestias eaque exercitationem. Excepturi quidem eveniet mollitia
+              ad dolorem iste.
+            </p>
+            <button>Read</button>
+          </div>
+        </div>
         <div className="storyCard">
           <img src="https://img.staticmb.com/mbcontent/images/uploads/2022/7/Image-of-colorful-sky-from-nandi-hills-bangalore.jpg" alt="" />
           <div className="content">
---
+++ pwBlog/client/src/Pages/AddBlog.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./addBlog.css"
+
+export const Addblog=()=>{
+    return(
+        <div id="main">
+            <h1>Write Your Blog</h1>
+            <input type="text" placeholder="Enter Title of Your Story" />
+            <input type="text" placeholder="Enter Url of Blog Image" />
+            <input type="text" placeholder="Type Short Description for your Blog" />
+
+            <textarea name="" id="" cols="30" rows="10" placeholder="Type Your Blog"></textarea>   
+            <button>Publish Blog</button>         
+            </div>
+    )
+}
